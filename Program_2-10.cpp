a5bd00c81f5aadd72c7b4f2114bc977526d7f0d1
aOudanonh <asakoud@gmail.com>
2021-01-25 17:13:03 +0000

    Program 2-10 (01/25/2021)

---
+++ Program_2-10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //declare and define variables
+    int checking = -20;
+    unsigned int miles = 4276;
+    long days = 189000;
+
+    //output statements
+    cout << "We have made a long journey of "
+         << miles
+         << " miles.\n"
+
+         << "Our checking account balance is "
+         << checking
+         << ".\n"
+         
+         << "About "
+         << days
+         << " days ago Columbus stood on this spot.\n";
+
+    return 0;
+}
